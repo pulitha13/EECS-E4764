5969e52b0749f21fd0848e6e934d816c46d04101
Joshua, Mathew <joshuagm12@gmail.com>
2024-09-30 16:27:49 -0400

    Working chkpt 1

--- Lab3/checkpoint_1.py
+++ Lab3/checkpoint_1.py
@@ -10,9 +10,9 @@ SDA_PIN = 4
 SCL_PIN = 5
 PIEZZO_PIN = 2
 BUTTON_PIN = 0
-OLED_BUTTON_A = 15
+OLED_BUTTON_A = 14
 OLED_BUTTON_B = 13
-OLED_BUTTON_C = 14
+OLED_BUTTON_C = 12
 
 debouncer = Debouncer()
 clock = Clock()
@@ -21,21 +21,21 @@ clock = Clock()
 def oled_a_handler(pin):
     global debouncer
     global clock
-
     # If button was pressed toggle change time mode
-    if(debouncer.get_debounced(pin) > 0):
+    if(debouncer.get_debounced(pin) == 0):
+        print("We in the oled_A_handler")
         clock.inc_clock_mode()
 
 
 def oled_b_handler(pin):
     global debouncer
     global clock
+    print("We in the oled_B_handler")
 
     # If button was pressed increase time dep on mode
-    if(debouncer.get_debounced(pin) > 0):
-
+    if(debouncer.get_debounced(pin) == 0):
         if(clock.edit_clock_mode == 1):
-            clock.inc_hours()
+            clock.inc_hour()
         elif(clock.edit_clock_mode == 2):
             clock.inc_min()
         elif(clock.edit_clock_mode == 3):
@@ -47,18 +47,17 @@ def oled_b_handler(pin):
 def oled_c_handler(pin):
     global debouncer
     global clock
+    print("We in the oled_C_handler")
 
-    # If button was pressed toggle change time mode
-    if(debouncer.get_debounced(pin) > 0):
-        match clock.edit_clock_mode:
-            case 1:
-                clock.dec_hours()
-            case 2:
-                clock.dec_min()
-            case 3:
-                clock.dec_sec()
-            case _:
-                return
+    if(debouncer.get_debounced(pin) == 0):
+        if(clock.edit_clock_mode == 1):
+            clock.dec_hour()
+        elif(clock.edit_clock_mode == 2):
+            clock.dec_min()
+        elif(clock.edit_clock_mode == 3):
+            clock.dec_sec()
+        else:
+            return
     
 
 def main():
@@ -68,43 +67,53 @@ def main():
     
     print("Initializing I2C and Display")
     i2c = I2C(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN))
-    display = ssd1306.SSD1306_I2C(128, 64, i2c)
+    display = ssd1306.SSD1306_I2C(128, 32, i2c)
 
     print("Initializing RTC")
-    rtc = RTC(clock.get_clock())
+    rtc = RTC()
+    year, month, day, weekday, hour, min, sec, subsec = (2024,9,27,5,12,12,12,0)
+    rtc.datetime()
 
     # Initialize pins
     print("Initializing Buttons")
-    oled_a = Pin(OLED_BUTTON_A)
-    oled_a.irq(handler=oled_a_handler, trigger=IRQ_FALLING)
-    oled_b = Pin(OLED_BUTTON_B)
-    oled_b.irq(handler=oled_b_handler, trigger=IRQ_FALLING)
-    oled_c = Pin(OLED_BUTTON_C)
-    oled_c.irq(handler=oled_c_handler, trigger=IRQ_FALLING)
+    oled_a = Pin(OLED_BUTTON_A, Pin.IN, Pin.PULL_UP)
+    oled_a.irq(handler=oled_a_handler, trigger=Pin.IRQ_FALLING)
+    oled_b = Pin(OLED_BUTTON_B, Pin.IN, Pin.PULL_UP)
+    oled_b.irq(handler=oled_b_handler, trigger=Pin.IRQ_FALLING)
+    oled_c = Pin(OLED_BUTTON_C, Pin.IN, Pin.PULL_UP)
+    oled_c.irq(handler=oled_c_handler, trigger=Pin.IRQ_FALLING)
 
     # Local variables to loop
     display_on = True
 
+
     while True:
+
         if clock.edit_clock_mode > 0:
 
             # Update the RTC based on the set time
-            rtc.datetime(clock.get_clock())
+            rtc.datetime((year, month, day, weekday, clock.hour, clock.min, clock.sec, subsec))
             # If we are any of the editing modes flash the screen on and off and upate clock
-            if display_on:
-                display.poweroff()
-            else:
-                display.poweron()
             display_on = not display_on
 
+            print(f"Setting Time as {clock.hour:02}:{clock.min:02}:{clock.sec:02} ")
+
         else:
+            display_on = True
             # If we are in normal display mode get RTC time and update clock module
-            (year, week, day, weekday, hours, min, sec, subsec) = rtc.datetime()
-            clock.set_clock(hours, min, sec)
-            print(f"Got Time as {clock.hours}:{clock.min}:{clock.sec} ")
-            display.text(f'{clock.hours}:{clock.min}:{clock.sec}')
+            year, month, day, weekday, hour, min, sec, subsec = rtc.datetime()
+            clock.set_clock(hour, min, sec)
+            print(f"Displaying {clock.hour:02}:{clock.min:02}:{clock.sec:02} ")
+        
+        if display_on:
+            display.poweron()
+        else:
+            display.poweroff()
 
         # Update clock every 1/4 sec
+        display_string = f'{clock.hour:02}:{clock.min:02}:{clock.sec:02}'
+        display.fill(0)
+        display.text(display_string,0,15,1)
         display.show()
         utime.sleep(0.25)
         
--- Lab3/clock_module.py
+++ Lab3/clock_module.py
@@ -4,13 +4,13 @@ class Clock:
     """
     Clock class defines a current time set by the variables
 
-    self.hours, self.min, self.sec
+    self.hour, self.min, self.sec
 
 
     and a self.clock mode with the following for modes
 
     0 - regular display time w/ no edit
-    1 - edit hours hand
+    1 - edit hour hand
     2 - edit minutes hand
     3 - edit seconds hand
 
@@ -19,7 +19,7 @@ class Clock:
     """
 
     def __init__(self):
-        self.hours = 0
+        self.hour = 0
         self.min = 0
         self.sec = 0
 
@@ -32,9 +32,9 @@ class Clock:
         self.edit_clock_mode = self.edit_clock_mode % 4
         return
     
-    def inc_hours(self):
-        self.hours += 1
-        self.hours = self.hours % 12
+    def inc_hour(self):
+        self.hour += 1
+        self.hour = self.hour % 24
 
     def inc_min(self):
         self.min += 1
@@ -44,20 +44,22 @@ class Clock:
         self.sec += 1
         self.sec = self.sec % 60
 
-    def dec_hours(self):
-        self.hours -= 1
-        self.hours = 12 if self.hours < 0 else self.hours
+    def dec_hour(self):
+        self.hour -= 1
+        self.hour = 23 if self.hour < 0 else self.hour
 
     def dec_min(self):
-        self.min += 1
-        self.min = 12 if self.min < 0 else self.min
+        self.min -= 1
+        self.min = 59 if self.min < 0 else self.min
 
     def dec_sec(self):
-        self.sec += 1
-        self.sec = 12 if self.sec < 0 else self.sec
+        self.sec -= 1
+        self.sec = 59 if self.sec < 0 else self.sec
 
     def get_clock(self):
-        return (0,0,0,0,self.hours,self.min,self.sec,0)
+        return ((0,0,0,0,self.hour,self.min,self.sec,0))
 
-    def set_clock(self, hours, min, sec):
-        return (0,0,0,0,self.hours,self.min,self.sec,0)
+    def set_clock(self, hour, min, sec):
+        self.sec = sec
+        self.min = min
+        self.hour = hour
